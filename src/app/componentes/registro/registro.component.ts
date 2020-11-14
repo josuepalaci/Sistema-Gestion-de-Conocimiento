6ab40598ad1845618bd6f6509d0162b094f883a8
Josue M <josuepalaci@gmail.com>
2020-11-13 18:20:18 -0600

    Registro de Usurios Funcionando

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  // CREDENCIALES 
+  email: string;
+  password: string;
+
+  constructor(
+    private flashMessages: FlashMessagesService,
+    private router: Router,
+    private logignService: LoginService
+  ) { }
 
   ngOnInit(): void {
   }
+
+  registro(){
+    this.logignService.registrarse(this.email, this.password)
+    .then( res => {
+      this.router.navigate(['/']);
+    }).catch( error => {
+      this.flashMessages.show(error.message, {
+        cssClass: 'alert-danger', timeout: 2500
+      })
+    })
+}
 
 }
